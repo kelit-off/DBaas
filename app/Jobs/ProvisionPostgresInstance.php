6ffd995363b213e835aab4dc9d8911a8e5f9b719
Théo Killian <theo.killian5700@gmail.com>
2026-01-04 22:33:40 +0100

    Correctif commande

--- app/Jobs/ProvisionPostgresInstance.php
+++ app/Jobs/ProvisionPostgresInstance.php
@@ -43,7 +43,7 @@ class ProvisionPostgresInstance implements ShouldQueue
     public function handle(): void
     {
 
-        $instanceSlug = Str::slug($this->project_data->name) . '-' . Str::random(6); // Crée une instance PostgreSQL avec Helm sur Kubernetes
+        $instanceSlug = Str::slug($this->project_data->name) . '-' . strtolower(Str::random(6));
         $process = Process::fromShellCommandline(sprintf(
             'KUBECONFIG=/etc/kubernetes/k3s.yaml helm install pg-%1$s bitnami/postgresql --namespace %1$s --create-namespace ' .
                 '--set auth.username=%2$s,auth.password=%3$s,auth.database=%4$s ' .
