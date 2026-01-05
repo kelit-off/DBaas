94156e188c39e277531e1801664d67df1b3550e1
Théo Killian <theo.killian5700@gmail.com>
2026-01-05 21:56:21 +0100

    Mettre cpu_core en string

--- app/Jobs/ProvisionPostgresInstance.php
+++ app/Jobs/ProvisionPostgresInstance.php
@@ -54,8 +54,8 @@ class ProvisionPostgresInstance implements ShouldQueue
             'helm install pg-%1$s bitnami/postgresql --namespace %1$s --create-namespace ' .
                 '--set auth.username=%2$s,auth.password=%3$s,auth.database=%4$s ' .
                 '--set primary.persistence.size=%5$s ' .
-                '--set resources.requests.memory=%6$s,resources.requests.cpu=%7$s ' .
-                '--set resources.limits.memory=%6$s,resources.limits.cpu=%7$s ' .
+                '--set-string resources.requests.memory=%6$s,resources.requests.cpu=%7$s ' .
+                '--set-string resources.limits.memory=%6$s,resources.limits.cpu=%7$s ' .
                 '--set metrics.enabled=true,metrics.service.port=9187,metrics.service.type=ClusterIP',
             $instanceSlug,
             $this->bd_data['username'],
